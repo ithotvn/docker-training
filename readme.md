30ce7794861bbc52ae28aae7689bed6ee6e1b529
Binh Ho <binh.ho@gmail.com>
2018-08-20 18:27:45 +0700

    First commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# ItHot.vn Training Docker with Docker PHP Nginx
+
+## Build docker
+
+    docker-compose up --build --force-recreate
---
+++ webapp/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ithot.vn.</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/custom.css">
+  </head>
+<body>
+
+<div class="block block--red-bg block--border">
+  <div class="block__heading">
+    <h1>Training Docker - ItHot.vn...Test volume OKKKKK</h1>
+  </div>
+  <div class="block__body">
+    <?php echo phpinfo(); ?>
+    <p>Training gulp sass...</p>
+  </div>
+</div>
+
+</body>
+</html>
